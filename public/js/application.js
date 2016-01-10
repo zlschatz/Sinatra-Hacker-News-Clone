df54686ccf8c2b047fe9b399039a3f940326ab91
Zach Schatz <zachschatz@gmail.com>
2016-01-10 12:01:25 -0500

    Remove original AJAX & vote partial

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
 
-  $("#up_vote").on("click", function(event){
-    event.preventDefault();
-    $.ajax({
-      type: 'get',
-      url : $(event.target).attr('href'),
-      data: $(this).serialize
-    }).done(function(response){
-      $("#up_vote").html(response);
-    });
-  });
 
 });
