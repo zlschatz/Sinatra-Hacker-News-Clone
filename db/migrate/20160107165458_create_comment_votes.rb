d22085d8c9e3c1e132614c6937f5ee40db1c1f86
Zach Schatz <zachschatz@gmail.com>
2016-01-12 12:20:18 -0500

    Working to complete comment votes

--- db/migrate/20160107165458_create_comment_votes.rb
+++ db/migrate/20160107165458_create_comment_votes.rb
@@ -2,7 +2,7 @@ class CreateCommentVotes < ActiveRecord::Migration
   def change
     create_table :comment_votes do |t|
       t.integer :vote
-      t.integer :post_id, null: false
+      t.integer :comment_id, null: false
       t.integer :user_id, null: false
 
       t.timestamps null: false
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $(".up_vote").on("click", function(event){
+  $("#news-feed").on("click", ".up_vote", function(event){
     event.preventDefault();
     var vote_count = $(event.target).parent().parent().find('#vote_count')
     $.ajax({
@@ -11,6 +11,22 @@ $(document).ready(function() {
       vote_count.text(response.vote_count);
     }).fail(function(){
       alert("Your vote has not been cast!");
+    });
+  });
+
+  $("#comment-container").on("click", ".up_vote", function(event){
+    event.preventDefault();
+    var target = $(event.target).parent().find("#vote_count")
+    // debugger
+    $.ajax({
+      type: "POST",
+      url : $(this).attr('href'),
+      dataType: "json",
+    }).done(function(response){
+      debugger
+      target.text(response.vote_count)
+    }).fail(function(response){
+      alert("Your comment cannot be posted");
     });
   });
 
